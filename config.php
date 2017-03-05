b0fb993f0791af9c6c5b2d70729110c7b52fbe50
Michail Kurockin <Michail.Kurachkin@promwad.com>
2017-03-05 15:41:52 +0300

    added global configurations

---
+++ config.php
@@ -0,0 +1,53 @@
+<?php
+require_once '/usr/local/lib/php/os.php';
+
+define("CONFIG_PATH", "/etc/sr90_automation/");
+
+function conf_db()
+{
+    $cfg_json = file_get_contents(CONFIG_PATH . 'database.json');
+    if (!$cfg_json) {
+        msg_log(LOG_ERR, sprintf("Can't open config file %s\n",
+                                 CONFIG_PATH . 'database.json'));
+        return null;
+    }
+    
+    $ret = json_decode($cfg_json);
+    if (!$ret) {
+        msg_log(LOG_ERR, sprintf("Can't parse config file %s\n",
+                                 CONFIG_PATH . 'database.json'));
+        return null;
+    }
+    
+    return (array)$ret;
+}
+
+function conf_io()
+{
+    return array("socket_file" => '/tmp/module_io_sock');
+}
+
+function conf_guard()
+{
+    return array('sirena_io_port' => 3,
+			     'lamp_io_port' => 4,
+                 'sirena_timeout' => 30, /* in seconds */
+			     'light_ready_timeout' => 60, /* in seconds */
+			     'light_sleep_timeout' => 30 * 60, /* in seconds */
+                 'camera_dir' => '/var/spool/guard_system/images',
+                 'video_cameras' => array(
+                                          array('id' => 1,
+                                                'v4l_dev' => '/dev/video14',
+                                                'resolution' => '1280:1024'),
+                                          array('id' => 2,
+                                                'v4l_dev' => '/dev/video15',
+                                                'resolution' => '1280:1024')),
+                 'phones' => array('+375295051024'),
+				);
+}
+
+
+function conf_modem()
+{
+    return array('ip_addr' => '192.168.1.1');
+}
