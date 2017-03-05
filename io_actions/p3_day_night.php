36e4caeb2b32cc9a27dbd5a727cfd0b0066bb86e
Michail Kurockin <Michail.Kurachkin@promwad.com>
2017-03-05 15:48:47 +0300

    io_actions: added day_night trigger

---
+++ io_actions/p3_day_night.php
@@ -0,0 +1,36 @@
+#!/usr/bin/php
+<?php
+
+require_once '/usr/local/lib/php/common.php';
+$utility_name = $argv[0];
+
+function main($argv) {
+    if (count($argv) < 3) {
+        printf("a few scripts parameters\n");
+        return 1;
+    }
+        
+    $port = $argv[1];
+    $port_state = $argv[2];
+    
+    $db = new Database;
+    $rc = $db->connect(conf_db());
+    if ($rc) {
+        printf("can't connect to database");
+        return 1;
+    }
+    
+    if ($port_state)
+        $state = 'day';
+    else
+        $state = 'night';
+        
+    if ($state == 'day')
+        sequncer_stop(conf_guard()['lamp_io_port']);
+    
+    $db->insert('day_night', array('state' => $state));
+    return 0;
+}
+
+
+return main($argv);
