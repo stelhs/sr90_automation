be8a9de1280cb5e6c4a345db6dd97e6125afc43b
Michail Kurockin <Michail.Kurachkin@promwad.com>
2017-03-05 15:47:01 +0300

    sms_actions: added sms command for guard system

---
+++ sms_actions/all_guard_commands.php
@@ -0,0 +1,47 @@
+#!/usr/bin/php
+<?php
+
+require_once '/usr/local/lib/php/common.php';
+require_once '/usr/local/lib/php/database.php';
+require_once '../config.php';
+require_once '../guard_lib.php';
+$utility_name = $argv[0];
+
+function main($argv) {
+    $rc = 0;
+    if (count($argv) < 4) {
+        printf("a few scripts parameters\n");
+        return -EINVAL;
+    }
+        
+    $sms_date = trim($argv[1]);
+    $phone = trim($argv[2]);
+    $sms_text = trim($argv[3]);
+    
+    $db = new Database;
+    $rc = $db->connect(conf_db());
+    if ($rc) {
+        printf("can't connect to database");
+        return -EBASE;
+    }
+    
+    switch ($cmd['cmd']) {
+    case 'off':
+        run_cmd("../guard.php state sleep sms");
+        break;    
+
+    case 'on':
+        guard_enable('sms');
+        break;    
+
+    default:
+        $undefined_text = 1;
+    }    
+    
+out:    
+    $db->close();
+    return $rc;
+}
+
+
+return main($argv);
