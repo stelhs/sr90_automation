42f5c812fa04dc984b4320466c80823073603d2d
stelhs <stelhs@ya.ru>
2019-05-01 14:48:56 +0300

    fixed internet connection switcher

--- inet_switch.sh
+++ inet_switch.sh
@@ -6,15 +6,23 @@ case "$1" in
 	1)
 		echo "switch to modem 1"
 		route delete default; route add default gw 192.168.1.1
+		iptables -F
+		iptables -X
 		iptables -F -t nat
-		iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source 192.168.1.100
+		iptables -X -t nat
+		iptables -t nat -A POSTROUTING -o enx582c80139263 -j SNAT --to-source 192.168.1.100
+		iptables -t mangle -A PREROUTING -i enx582c80139263 -j TTL --ttl-set 64
 	;;
 
 	2)
 		echo "switch to modem 2";
-                route delete default; route add default gw 192.168.2.1
-                iptables -F -t nat
-                iptables -t nat -A POSTROUTING -o p1p1 -j SNAT --to-source 192.168.2.2
+		route delete default; route add default gw 192.168.2.1
+		iptables -F
+		iptables -X
+		iptables -F -t nat
+		iptables -X -t nat		
+		iptables -t nat -A POSTROUTING -o enp2s0 -j SNAT --to-source 192.168.2.2
+		iptables -t mangle -A PREROUTING -i enp2s0 -j TTL --ttl-set 64
 	;;
 	
 	*)
