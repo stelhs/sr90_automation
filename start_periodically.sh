8559ea4e8e29dfc324be1f0adc7ad34ae5b99175
stelhs <stelhs@ya.ru>
2019-08-18 10:12:25 +0300

    implement STOP periodically

--- start_periodically.sh
+++ start_periodically.sh
@@ -3,6 +3,11 @@
 cd "$(dirname "$0")"
 while [ 1 ]
 do
-	for file in periodically/*; do ./$file auto; done
+	if [ ! -e periodically/STOP ]
+	then
+		for file in periodically/*; do
+			./$file auto; 
+		done
+	fi
 	sleep 1;
 done
